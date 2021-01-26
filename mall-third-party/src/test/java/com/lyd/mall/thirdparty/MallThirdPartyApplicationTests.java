dd1705e1cc68f45ec9708240e0318822af0b356c
Liuyunda <man021436@163.com>
2021-01-26 23:45:07 +0800

    1.dev分支代码合并

---
+++ mall-third-party/src/test/java/com/lyd/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -0,0 +1,65 @@
+package com.lyd.mall.thirdparty;
+
+import com.aliyun.oss.OSSClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest
+class MallThirdPartyApplicationTests {
+    @Resource
+    OSSClient ossClient;
+    @Test
+    void contextLoads() {
+    }
+    /**
+     * @Description: 阿里云原生sdk上传文件流
+     * @Param: []
+     * @return: void
+     * @Author: Liuyunda
+     * @Date: 2021/1/18
+     */
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        // String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+        // String accessKeyId = "LTAI4GCdBUWRVWTyQjxwT5hU";
+        // String accessKeySecret = "oqCP5jnqIKvz45kSaCsn8hlvEwVSZY";
+        //
+        // // 创建OSSClient实例。
+        // OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\ME\\avatar.jpg");
+        ossClient.putObject("mall-lyd", "avatar.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传完成");
+    }
+
+    /**
+     * @Description: springcloud-alibaba oos 快速上传
+     * @Param: []
+     * @return: void
+     * @Author: Liuyunda
+     * @Date: 2021/1/18
+     */
+    @Test
+    public void testUpload2() throws FileNotFoundException {
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\ME\\hero.png");
+        ossClient.putObject("mall-lyd", "mm.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传完成");
+    }
+}
