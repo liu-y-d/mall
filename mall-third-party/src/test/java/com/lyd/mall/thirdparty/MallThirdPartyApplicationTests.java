aec9e61f157bbdd53c66cbcf282017ae9dab0858
LiuYunDa <51875157+SexyJava@users.noreply.github.com>
2021-03-16 12:55:59 +0800

    Update MallThirdPartyApplicationTests.java

--- mall-third-party/src/test/java/com/lyd/mall/thirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/com/lyd/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -28,8 +28,6 @@ class MallThirdPartyApplicationTests {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         // String endpoint = "oss-cn-beijing.aliyuncs.com";
         // // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        // String accessKeyId = "LTAI4GCdBUWRVWTyQjxwT5hU";
-        // String accessKeySecret = "oqCP5jnqIKvz45kSaCsn8hlvEwVSZY";
         //
         // // 创建OSSClient实例。
         // OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
