127770d7be471b2ba4ee15fabb9031328da1f566
Liuyunda <man021436@163.com>
2021-03-22 23:24:07 +0800

    1.商城首页-thymeleaf模版及静态资源

---
+++ mall-product/src/main/java/com/lyd/mall/product/app/SkuSaleAttrValueController.java
@@ -0,0 +1,84 @@
+package com.lyd.mall.product.app;
+
+import java.util.Arrays;
+import java.util.Map;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lyd.mall.product.entity.SkuSaleAttrValueEntity;
+import com.lyd.mall.product.service.SkuSaleAttrValueService;
+import com.lyd.common.utils.PageUtils;
+import com.lyd.common.utils.R;
+
+
+/**
+ * sku销售属性&值
+ *
+ * @author liuyunda
+ * @email man021436@163.com
+ * @date 2020-12-28 22:35:46
+ */
+@RestController
+@RequestMapping("product/skusaleattrvalue")
+public class SkuSaleAttrValueController {
+    @Autowired
+    private SkuSaleAttrValueService skuSaleAttrValueService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    public R list(@RequestParam Map<String, Object> params) {
+        PageUtils page = skuSaleAttrValueService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    public R info(@PathVariable("id") Long id) {
+        SkuSaleAttrValueEntity skuSaleAttrValue = skuSaleAttrValueService.getById(id);
+
+        return R.ok().put("skuSaleAttrValue", skuSaleAttrValue);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    public R save(@RequestBody SkuSaleAttrValueEntity skuSaleAttrValue) {
+        skuSaleAttrValueService.save(skuSaleAttrValue);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    public R update(@RequestBody SkuSaleAttrValueEntity skuSaleAttrValue) {
+        skuSaleAttrValueService.updateById(skuSaleAttrValue);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    public R delete(@RequestBody Long[] ids) {
+        skuSaleAttrValueService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}
