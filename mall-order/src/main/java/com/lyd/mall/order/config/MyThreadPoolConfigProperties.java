829bf9b770dd70ac9d3f3121582b8e93917f8246
LiuYunDa <51875157+SexyJava@users.noreply.github.com>
2021-06-01 22:36:47 +0800

    订单服务-整合springsession

---
+++ mall-order/src/main/java/com/lyd/mall/order/config/MyThreadPoolConfigProperties.java
@@ -0,0 +1,20 @@
+package com.lyd.mall.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author Liuyunda
+ * @Date 2021/5/12 21:57
+ * @Email man021436@163.com
+ * @Description: TODO
+ */
+@ConfigurationProperties(prefix = "mall.thread")
+@Component
+@Data
+public class MyThreadPoolConfigProperties {
+    private Integer coreSize;
+    private Integer maxSize;
+    private Integer keepAliveTime;
+}
